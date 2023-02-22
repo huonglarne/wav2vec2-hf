ec4295a0c1e6eccdce0c6beafdbe265df447ff6c
“Kim <kim.nguyen@moreh.com.vn>
2023-02-22 04:02:54 +0000

    idk

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wav2vec2-hf
 wav2vec2
+
+export HF_DATASETS_CACHE='/nas/tmp'
--- wav2vec2_inference.py
+++ wav2vec2_inference.py
@@ -1,33 +1,37 @@
+
 from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC, Wav2Vec2PhonemeCTCTokenizer
 from datasets import load_dataset
 import torch
- 
-# load model and processor
-# processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-960h")
 
-# model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h")
-# model = model.cuda()
+import soundfile as sf
+import librosa
 
-processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-lv-60-espeak-cv-ft")
-model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-lv-60-espeak-cv-ft")
-# model = model.cuda()
+speech_processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-960h")
+speech_model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h")
+speech_model = speech_model.cuda()
+
+phoneme_processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-lv-60-espeak-cv-ft")
+phoneme_model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-lv-60-espeak-cv-ft")
+phoneme_model = phoneme_model.cuda()
     
+def get_transcript(processor, model, input):
+    input_values = processor(input, return_tensors="pt", padding="longest").input_values  # Batch size 1
+    input_values = input_values.cuda()
+    logits = model(input_values).logits
+    predicted_ids = torch.argmax(logits, dim=-1)
+    transcription = processor.batch_decode(predicted_ids)
+
+    return transcription
     
 # load dummy dataset and read soundfiles
-ds = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation")
+# ds = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation")
+# input = ds[0]["audio"]["array"]
 
-# tokenize
-input_values = processor(ds[0]["audio"]["array"], return_tensors="pt", padding="longest").input_values  # Batch size 1
-# input_values = input_values.cuda()
+input, _ = librosa.load("data/audio/Read83.m4a", sr = 16000)
 
-# retrieve logits
-logits = model(input_values).logits
+speech = get_transcript(speech_processor, speech_model, input)
+phoneme = get_transcript(phoneme_processor, phoneme_model, input)
 
-# # take argmax and decode
-# predicted_ids = torch.argmax(logits, dim=-1)
-# transcription = processor.batch_decode(predicted_ids)
+print(speech)
 
-# print(transcription)
-
-# # ['b ɪ k ʌ z j uː ɚ s l iː p ɪ ŋ ɪ n s t ɛ d ʌ v k 
-# # ɑː ŋ k ɚ ɹ ɪ ŋ ð ə l ʌ v l i ɹ oʊ z p ɹ ɪ n s ɛ s h ɐ z b ɪ k ʌ m ɐ f ɪ d əl w ɪ ð aʊ ɾ ɐ b oʊ w ɑː l p ɔːɹ ɹ ʃ æ ɡ i s ɪ t s ð ɛ ɐ k uː ɪ ŋ d ʌ v']
+print(phoneme)
